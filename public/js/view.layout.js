df263fdaf8d2c1533f1eae7249287623a687ecc6
Max Li <maxli@Maxs-MacBook-Pro.local>
2017-04-13 18:30:49 -0700

    Update mobile viewport and mobile menu

--- public/js/view.layout.js
+++ public/js/view.layout.js
@@ -11,14 +11,25 @@ $(function () {
   };
   var header  = new Headhesive('.secondary.menu', options);
 
-  $(document).on('click', '.popupHelp', function() {
+  $(document).on('click', '.popupHelp', function () {
     $('.helpModal')
       .modal('show');
   });
 
   //modal close
-  $(document).on('click', '.closeModal', function() {
+  $(document).on('click', '.closeModal', function () {
     $('.helpModal')
       .modal('hide all');
+  })
+
+  //sidebar open
+  $('.fa-bars').click(function () {
+    $('.sidebar')
+      .sidebar('show')
+  })
+  //sidebar close
+  $('.sidebar-close').click(function () {
+    $('.sidebar')
+      .sidebar('hide')
   })
 });
